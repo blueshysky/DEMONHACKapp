8c0cb4114112e26462c137b3d399b26738b2b09b
Vincent <xhuang49@hawk.iit.edu>
2018-10-20 17:58:20 -0500

    edited vincent version of this

---
+++ public/test.js
@@ -0,0 +1,25 @@
+
+var config = {
+  apiKey: "AIzaSyAbnjkipv2m1ZLl5ay2yuH2fIMmkVwfuRA",
+  authDomain: "demonhackdatabase.firebaseapp.com",
+  databaseURL: "https://demonhackdatabase.firebaseio.com",
+  projectId: "demonhackdatabase",
+  storageBucket: "demonhackdatabase.appspot.com",
+  messagingSenderId: "551307232003"
+};
+firebase.initializeApp(config);
+
+
+var btnlogin = document.getElementById('btnlogin');
+
+btnlogin.onclick = function(){
+  // var database = firebase.database();
+  // var encodedImg = database.ref('pngs');
+  // var result = encodedImg.push({
+  //     password :'password',
+  //     coins : '0'
+  //   });
+    console.log(123);
+  };
+
+  console.log('default');
